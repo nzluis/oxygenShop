e794855b18d4b94a1b0fe874a38f6e211b5e66ff
luis <luisnavarroFS@gmail.com>
2024-02-05 12:44:57 +0100

    feat: form validation

--- script.js
+++ script.js
@@ -45,12 +45,11 @@ function handleScroll() {
     const scrolled = (windowScroll / height) * 100;
   
     document.querySelector(".navbar__scrollBar").style.width = scrolled + "%";
-    console.log(scrolled)
 }
 
 //---------------------------------------------------
 
-//////////////////SCROLL %//////////////////
+//////////////////RETURN TO TOP//////////////////
 
 const returnTopButton = document.querySelector('.returnTop')
 
@@ -60,4 +59,74 @@ function handleReturnTop() {
     document.documentElement.scrollTop = 0
 }
 
+//---------------------------------------------------
+
+//////////////////FORM VALIDATION//////////////////
+const form = document.querySelector(".contactForm__form")
+let email = document.querySelector(".contactForm__form__email")
+const form_name = document.querySelector(".contactForm__form__name")
+const checkbox = document.querySelector(".contactForm__form__consent__checkbox")
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+form.addEventListener('submit', (event) => {
+    
+    if (!checkbox.checked) {
+        checkbox.classList.add("invalid")
+        event.preventDefault()
+    }
+
+    if (form_name.value.length < 2 || form_name.value.length > 100) {
+        form_name.classList.add("input-invalid")
+        event.preventDefault()
+    }
+
+    if (!emailRegex.test(email.value) ){
+        console.log(emailRegex.test(email.value))
+        email.classList.add("input-invalid")
+        event.preventDefault()
+    } else {
+        email.classList.remove("input-invalid")
+    }
+})
+
+form_name.addEventListener('input', () => {
+
+    if (form_name.value.length < 2 || form_name.value.length > 100) {
+        form_name.classList.add("input-invalid")
+        form_name.setCustomValidity("Name must contain at least 2 characters")
+    } else {
+        form_name.setCustomValidity("")
+        form_name.classList.remove("input-invalid")
+    }
+})
+
+checkbox.addEventListener("input", () => {
+
+    if (checkbox.checked) {
+        checkbox.classList.remove("invalid")
+        checkbox.setCustomValidity("")
+    } else {
+        checkbox.setCustomValidity("Please click here")
+        checkbox.classList.add("invalid")
+    }
+});
+
+email.addEventListener('input', () => {
+    
+    if(emailRegex.test(email.value)) {
+        email.setCustomValidity("");
+        email.classList.remove("input-invalid")
+    } else {
+        email.setCustomValidity("A proper email name, please");
+        email.classList.add("input-invalid")
+    }
+})
+//---------------------------------------------------
+
+//////////////////FETCH JSON//////////////////
+//---------------------------------------------------
+//////////////////POPUP MODAL//////////////////
+//---------------------------------------------------
+//////////////////CURRENCY SELECTOR//////////////////
+//---------------------------------------------------
+//////////////////SLIDER//////////////////
 //---------------------------------------------------
